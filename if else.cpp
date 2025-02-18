cd2c9805528dffcde877b43b1b809c8fc4beaf68
Ali-007-gr <aliahmad4239991@gmail.com>
2025-02-19 00:31:06 +0500

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	int age;
+	cout<<"Enter your age = ";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"ou are Eigible to vote";
+	}
+	else
+	{
+		cout<<"You are not Eligible to vote";
+	}
+	return 0;
+}
